567cf2dbca61f54946d50aca45552dbdefe799ed
unknown <thomas.vanvelthooven@gmail.com>
2021-04-06 22:09:23 +0200

    Initial commit

---
+++ scrips/Index.js
@@ -0,0 +1,11 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.body;
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+};
+
+lightButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+};
