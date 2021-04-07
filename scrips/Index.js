f52f40d3a8a3b1d5d1ed0a2996d80ed5dab14f82
blackdragon2447 <56592338+blackdragon2447@users.noreply.github.com>
2021-04-07 12:36:44 +0200

    remove dark light toggle (temp?)

--- scrips/Index.js
+++ scrips/Index.js
@@ -1,11 +1,11 @@
 const darkButton = document.getElementById('dark');
-const lightButton = document.getElementById('light');
+let darkmode = true;
 const body = document.body;
 
 darkButton.onclick = () => {
-    body.classList.replace('light', 'dark');
-};
-
-lightButton.onclick = () => {
-    body.classList.replace('dark', 'light');
+    if(darkmode){
+        body.classList.replace('light', 'dark');
+    } else {
+        body.classList.replace('dark', 'light')
+    }
 };
